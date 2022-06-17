215ab005c9c00641faa192d0b2472bdbd71b7e06
hermanmans <hsmans0601@gmail.com>
2022-06-17 08:02:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,18 +6,16 @@ class Task{
     //return this; is implicit with Constructor Functions
   }
   } 
-    
-    
-    
-    let array1 = [];
+
     const btnClicked = document.getElementById("submit1");
-  
     btnClicked.addEventListener("click",() =>{
-      var li = document.createElement("li");
+      let li = document.createElement("li");
       let taskName = document.getElementById("taskInput").value;
       let dueDate = document.getElementById("dateInput").value;
-      var x = document.createTextNode(li);
+      let x = document.createTextNode(taskName);
+      let y = document.createTextNode(dueDate);
       li.appendChild(x);
+      li.appendChild(y);
       
       const task1 = new Task(taskName,dueDate);
   //create more instances of this task using a if statement and submission counter.
@@ -38,9 +36,10 @@ class Task{
       //document.getElementById("output3").innerHTML=task2._firstname;
       //document.getElementById("output4").innerHTML=task2._lastname;
       
-      array1.push(task1._firstname); // BAD practice. Use get method to include privacy task.firstname//
-      console.log(array1);
-      console.log(array1.length);
+      // BAD practice. Use get method to include privacy task.firstname//
+      //console.log(array1);
+      //console.log(array1.length);
+      //console.log(task2);
     });
   
     
