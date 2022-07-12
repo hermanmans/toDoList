fb4ac192aa05f39ede5db6a23dbc1e6f953527e5
hermanmans <hsmans0601@gmail.com>
2022-07-12 12:07:13 +0200

    12/07 12:07

--- toDoListUpd.js
+++ toDoListUpd.js
@@ -7,18 +7,34 @@ class Task{
     };
   };
   
-  class Tags extends Task {
-    constructor(title, description,date_created,date_due, tagname){
-        super(title, description,date_created,date_due);
-        this._tagname = tagname;
-        this._defaultTagName = 'general';
-    };
-        get tagname(){
+class Tags extends Task {
+  constructor(title, description,date_created,date_due, tagname){
+      super(title, description,date_created,date_due);
+      this._tagname = tagname;
+      this._defaultTagName = "general"
+  };
+      get tagname(){
         return this._tagname;
-        };
-  
+      };
+
+      get "general"(){
+        return this._defaultTagName;
+      };
+
+};
+
+class Status extends Tags{
+  constructor(title, description,date_created,date_due, tagname,){
+    super(title, description,date_created,date_due, tagname);
   };
 
-  let todo = new Tags();
+  static status (completed){
+    this._completed = completed;
+    return completed;
+  };
 
-  console.log(todo);
+}
+
+
+let todo = new Status('Head to Gym', 'Someting that I should turn into an habit', 2018-04-24, 2020-12-06, ['exercise', 'fitness', ' routines'], '');
+let complete = Status.status("YES");
